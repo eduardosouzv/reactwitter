9a1ffd015b320d0d34ce8c5be427d41d20084dfd
eduardo <=>
2022-03-20 16:12:30 -0300

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,27 @@
+import { Container, Trending } from './styles';
+
 import MenuBar from '../../components/MenuBar';
-import { Container } from './styles';
+import Feed from '../../components/Feed';
+import Topic from '../../components/Topic';
 
 export default function Home() {
   return (
     <Container>
       <MenuBar />
-      <div>feed</div>
+      <Feed />
+      <Trending>
+        <h1>Trending</h1>
+        <Topic
+          category="Development"
+          title="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          volumnOfTweets={3}
+        />
+        <Topic
+          category="Development"
+          title="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          volumnOfTweets={3}
+        />
+      </Trending>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,5 +2,21 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
   backgroung-color: red;
+  margin: 0 auto;
+`;
+
+export const Trending = styled.div`
+  background-color: ${({ theme }) => theme.colors.darkGray};
+  margin: 24px;
+  border-radius: 24px;
+  min-width: 400px;
+  max-height: 600px;
+
+  overflow: auto;
+
+  h1 {
+    padding: 18px;
+  }
 `;
