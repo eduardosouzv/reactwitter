586306de9160e7131ee0c7d9bc7b90bddf3f1035
eduardo <eduardosouzv@gmail.com>
2022-04-04 19:52:33 -0300

    fix: remove trending component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,13 @@
-import { Container, Trending } from './styles';
+import { Container } from './styles';
 
 import MenuBar from '../../components/MenuBar';
 import Feed from '../../components/Feed';
-import Topic from '../../components/Topic';
 
 export default function Home() {
   return (
     <Container>
       <MenuBar />
       <Feed />
-      <Trending>
-        <h1>Trending</h1>
-        <Topic
-          category="Development"
-          title="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          volumnOfTweets={3}
-        />
-        <Topic
-          category="Development"
-          title="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          volumnOfTweets={3}
-        />
-      </Trending>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,17 +6,3 @@ export const Container = styled.div`
   backgroung-color: red;
   margin: 0 auto;
 `;
-
-export const Trending = styled.div`
-  background-color: ${({ theme }) => theme.colors.darkGray};
-  margin: 24px;
-  border-radius: 24px;
-  min-width: 400px;
-  max-height: 600px;
-
-  overflow: auto;
-
-  h1 {
-    padding: 18px;
-  }
-`;
