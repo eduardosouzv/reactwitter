45cbad3dc8f6aa7188c9e99763c958ed8eb9c6ee
Eduardo <eduardosouzv@gmail.com>
2022-04-19 22:54:35 -0300

    removed styled files

--- src/assets/styles/themes/default.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  colors: {
-    blue: '#1DA1F2',
-    black: '#080404',
-    darkGray: '#181c1c',
-    lightGray: '#AAB8C2',
-    white: '#e7e9ea',
-  },
-};
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,19 +1,13 @@
 import { ApolloProvider } from '@apollo/client';
-import { ThemeProvider } from 'styled-components';
 
-import defaultTheme from '../../assets/styles/themes/default';
-import GlobalStyles from '../../assets/styles/global';
 import client from '../../services/utils/graphqlClient';
 
 import Home from '../../pages/Home';
 
 export default function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <ApolloProvider client={client}>
-        <Home />
-        <GlobalStyles />
-      </ApolloProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
   );
 }
