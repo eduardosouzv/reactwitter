5bb64872d5793cc63b1e0f14a4de7507cf8156ce
Eduardo <eduardosouzv@gmail.com>
2022-04-24 18:48:28 -0300

    login modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import MenuBar from '../../components/MenuBar';
 import Feed from '../../components/Feed';
+import { useModal } from '../../hooks/useModal';
 
 export default function Home() {
+  const { LoginModal } = useModal();
+
   return (
     <div className="flex justify-center my-0 mx-auto">
       <MenuBar />
       <Feed />
+      {LoginModal}
     </div>
   );
 }
