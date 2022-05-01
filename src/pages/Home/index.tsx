85e894b0ba7c45f336b1625abb50df9d7aab9c9c
Eduardo <eduardosouzv@gmail.com>
2022-05-01 17:40:07 -0300

    remove modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,13 +3,10 @@ import Feed from '../../components/Feed';
 import { useModal } from '../../hooks/useModal';
 
 export default function Home() {
-  const { LoginModal } = useModal();
-
   return (
     <div className="flex justify-center my-0 mx-auto">
       <MenuBar />
       <Feed />
-      {LoginModal}
     </div>
   );
 }
